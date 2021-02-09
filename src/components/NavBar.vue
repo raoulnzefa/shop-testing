481fafc6abfe4694860dfddecf1a8820da295c80
Etienne Dayre <etiennedayre10@gmail.com>
2021-02-09 22:39:48 +0100

    feat: create new component for render svg

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,13 @@
           class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start"
         >
           <div class="flex items-center flex-shrink-0">
-            <HomeSvg classes="block w-auto h-8 text-white fill-current" />
+            <IconBase
+              classe="block w-auto h-8 text-white fill-current"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              ><path
+                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+            /></IconBase>
           </div>
           <div class="hidden sm:flex sm:ml-6">
             <NavLink />
@@ -25,21 +31,16 @@
               @click.prevent="(isNotif = !isNotif), (isOpen = false)"
             >
               <span class="sr-only">View notifications</span>
-              <svg
-                class="w-6 h-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
+              <IconBase
+                classe="w-6 h-6"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
+                ><path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
+              /></IconBase>
             </button>
             <div
               class="absolute right-0 py-1 mt-2 origin-top-right bg-white border border-gray-300 rounded-lg shadow-lg w-max"
@@ -49,17 +50,13 @@
             >
               <div class="flex flex-row">
                 <div class="px-2">
-                  <svg
-                    width="24"
-                    height="24"
+                  <IconBase
+                    classe="w-6 h-6"
                     viewBox="0 0 1792 1792"
                     fill="#44C997"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
+                    ><path
                       d="M1299 813l-422 422q-19 19-45 19t-45-19l-294-294q-19-19-19-45t19-45l102-102q19-19 45-19t45 19l147 147 275-275q19-19 45-19t45 19l102 102q19 19 19 45t-19 45zm141 83q0-148-73-273t-198-198-273-73-273 73-198 198-73 273 73 273 198 198 273 73 273-73 198-198 73-273zm224 0q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"
-                    />
-                  </svg>
+                  /></IconBase>
                 </div>
                 <div class="ml-2 mr-6 text-sm text-left">
                   <span class="font-semibold"
@@ -72,25 +69,17 @@
               </div>
               <div class="flex flex-row">
                 <div class="px-2">
-                  <svg
-                    width="24"
-                    height="24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 455.111 455.111"
-                  >
-                    <circle
+                  <IconBase viewBox="0 0 455.111 455.111" fill="#44C997"
+                    ><circle
                       style="fill: #e24c4b"
                       cx="227.556"
                       cy="227.556"
-                      r="227.556"
-                    />
+                      r="227.556" />
                     <path
                       style="fill: #d1403f"
                       d="M455.111,227.556c0,125.156-102.4,227.556-227.556,227.556c-72.533,0-136.533-32.711-177.778-85.333
 	c38.4,31.289,88.178,49.778,142.222,49.778c125.156,0,227.556-102.4,227.556-227.556c0-54.044-18.489-103.822-49.778-142.222
-	C422.4,91.022,455.111,155.022,455.111,227.556z"
-                    />
+	C422.4,91.022,455.111,155.022,455.111,227.556z" />
                     <path
                       style="fill: #ffffff"
                       d="M331.378,331.378c-8.533,8.533-22.756,8.533-31.289,0l-72.533-72.533l-72.533,72.533
@@ -98,8 +87,7 @@
 	c-8.533-8.533-8.533-22.756,0-31.289c8.533-8.533,22.756-8.533,31.289,0l72.533,72.533l72.533-72.533
 	c8.533-8.533,22.756-8.533,31.289,0c8.533,8.533,8.533,22.756,0,31.289l-72.533,72.533l72.533,72.533
 	C339.911,308.622,339.911,322.844,331.378,331.378z"
-                    />
-                  </svg>
+                  /></IconBase>
                 </div>
                 <div class="ml-2 mr-6 text-sm text-left">
                   <span class="font-semibold">Erreur commande #X3910</span>
@@ -119,18 +107,15 @@
               @click.prevent="(isOpen = !isOpen), (isNotif = false)"
             >
               <span class="sr-only">Open user menu</span>
-              <svg
-                class="w-8 h-8 text-white rounded-full fill-current"
-                xmlns="http://www.w3.org/2000/svg"
+              <IconBase
+                classe="w-8 h-8 text-white rounded-full fill-current"
                 viewBox="0 0 20 20"
                 fill="currentColor"
-              >
-                <path
+                ><path
                   fill-rule="evenodd"
                   d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                   clip-rule="evenodd"
-                />
-              </svg>
+              /></IconBase>
             </button>
             <div
               class="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
@@ -166,8 +151,8 @@
 </template>
 
 <script setup>
-import HomeSvg from "./NavBarItems/HomeSvg.vue";
 import NavLink from "./NavBarItems/NavLink.vue";
+import IconBase from "./NavBarItems/IconBase.vue";
 </script>
 
 <script>
@@ -189,7 +174,7 @@ export default {
   mounted() {
     document.addEventListener("click", this.close);
   },
-  beforeDestroy() {
+  beforeUnmount() {
     document.removeEventListener("click", this.close);
   },
 };
---
+++ src/components/NavBarItems/IconBase.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg
+    :class="classe"
+    :width="width"
+    :height="height"
+    :viewBox="viewBox"
+    :fill="fill"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <slot />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    classe: {
+      type: String,
+      default: "classes",
+    },
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+    viewBox: {
+      type: String,
+      default: "0 0 24 24",
+    },
+    fill: {
+      type: String,
+      default: "none",
+    },
+  },
+};
+</script>
